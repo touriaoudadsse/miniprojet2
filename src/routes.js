85c93e8619fc8d84015e5b226ef6736fcc11d803
miranto <miranto.razafindrazaka@epitech.eu>
2019-09-12 20:11:01 +0200

    complete routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from "./components/Home";
 import User from "./components/User";
+import WorkingTime from "./components/WorkingTime";
+import WorkingTimes from "./components/WorkingTimes";
+import ClockManager from "./components/ClockManager";
+import ChartManager from "./components/ChartManager";
 
 Vue.use(VueRouter);
 
@@ -17,6 +21,27 @@ let router = new VueRouter({
             path: '/user',
             component: User,
             name: 'user'
+        },
+        {
+            path: '/workingTime/:userid',
+            alias: '/workingTime/:userid/:workingtimeid',
+            component: WorkingTime,
+            name: 'workingtime'
+        },
+        {
+            path: '/workingTimes/:userid',
+            component: WorkingTimes,
+            name: 'workingtimes'
+        },
+        {
+            path: '/clock/:username',
+            component: ClockManager,
+            name: 'clockmanager'
+        },
+        {
+            path: '/chartManager/:userid',
+            component: ChartManager,
+            name: 'chartManager'
         },
         {
             path: '*',
