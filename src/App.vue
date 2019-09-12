ce18629ceb1a532a8b1eec451e069f4598d68fda
touria <touria.oudadsse@epitech.eu>
2019-09-12 10:53:12 +0200

    miniprojet2
    add new fc

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info" >
+    <b-navbar-brand href="#">TeamTimeWeb</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> 
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      </b-collapse>  
+  </b-navbar>
+ <!-- <div>
+    <b-button-group>
+      <b-button>Current User</b-button>
+    </b-button-group>
+  </div>-->
+  <div>
+  <div class="mx-auto bg-info" style="width: 500px;">
+</div>
+ <div>
+  <b-tabs content-class="mt-3">
+    <!--Current User -->
+    <div class="mx bg-info" style="width: 8000px;">
+      <b-tab title="Current User" active>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+        <b-form-group id="input-group-2" label="Email :" label-for="input-2">
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="username :" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+ <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    
+      
+    </b-form>
+    
+     
+    </b-card>
+    </b-tab>
+    </div>
+    <b-tab title="User list">
+      <p> </p>
+      </b-tab>
+   <b-tab title="Working Time">
+     <p> </p>
+     </b-tab>
+    <b-tab title="Clock Manager">
+      <p> </p>
+      </b-tab>
+   <b-tab title="Chart Manager">
+     <p> </p>
+     </b-tab>
+  
+  </b-tabs>
+</div>
+
+</div>
+
+<template>
+  <div>
+  
+      
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+
+
+
+
+</div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: ''
+        },
+       
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+  
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { ModalPlugin } from 'bootstrap-vue'
+Vue.use(ModalPlugin)
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { CardPlugin } from 'bootstrap-vue'
+Vue.use(CardPlugin)
+
+// This imports directive v-b-scrollspy as a plugin:
+import { VBScrollspyPlugin } from 'bootstrap-vue'
+Vue.use(VBScrollspyPlugin)
+
+// This imports the dropdown and table plugins
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
