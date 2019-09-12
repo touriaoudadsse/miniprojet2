885de5c864ba64cae366726dad28bb4733cbd10b
miranto <miranto.razafindrazaka@epitech.eu>
2019-09-12 21:10:01 +0200

    Missing component again

---
+++ src/components/ChartManager.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="chartmanager">
+        <donut-chart
+                id="donut"
+                :data="donutData"
+                colors='[ "#FF6384", "#36A2EB", "#FFCE56" ]'
+                resize="true">
+        </donut-chart>
+    </div>
+</template>
+
+<script>
+    import Raphael from 'raphael/raphael';
+
+    global.Raphael = Raphael;
+
+    import {DonutChart} from 'vue-morris';
+
+    export default {
+        name: "ChartManager",
+        data: function () {
+            return {
+                donutData: [
+                    {label: 'Red', value: 300},
+                    {label: 'Blue', value: 50},
+                    {label: 'Yellow', value: 100}
+                ]
+            }
+        },
+        components: {
+            DonutChart
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
